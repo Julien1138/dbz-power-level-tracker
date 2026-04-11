b293f668eb3436fb52d138e701bb69e7ba0e54cf
Julien Aupart <julien.aupart@gmail.com>
2026-04-11 15:16:56 +0200

    Update Bardock image resources and refactor display logic for platform compatibility

--- src/c/bardock.c
+++ src/c/bardock.c
@@ -1,33 +1,17 @@
 #include "bardock.h"
 
-#if defined(PBL_PLATFORM_EMERY)
-#define BARDOCK_RES RESOURCE_ID_IMAGE_BARDOCK_0_X3
-#define BARDOCK_X -100
-#elif defined(PBL_PLATFORM_GABBRO)
-#define BARDOCK_RES RESOURCE_ID_IMAGE_BARDOCK_0_X3
-#define BARDOCK_X -80
-#elif defined(PBL_PLATFORM_CHALK)
-#define BARDOCK_RES RESOURCE_ID_IMAGE_BARDOCK_0_X2
-#define BARDOCK_X -50
-#else
-#define BARDOCK_RES RESOURCE_ID_IMAGE_BARDOCK_0_X2
-#define BARDOCK_X -70
-#endif
-
-#define BARDOCK_Y_OFFSET 0 // px from bottom: 0 = flush, + = below screen, - = above
-
 static BitmapLayer *s_layer;
 static GBitmap *s_bitmap;
 
 void bardock_create(Layer *root)
 {
-  s_bitmap = gbitmap_create_with_resource(BARDOCK_RES);
+  s_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BARDOCK);
 
   GSize size = gbitmap_get_bounds(s_bitmap).size;
   int screen_h = layer_get_bounds(root).size.h;
-  int y = screen_h - size.h + BARDOCK_Y_OFFSET;
+  int y = screen_h - size.h;
 
-  s_layer = bitmap_layer_create(GRect(BARDOCK_X, y, size.w, size.h));
+  s_layer = bitmap_layer_create(GRect(0, y, size.w, size.h));
   bitmap_layer_set_bitmap(s_layer, s_bitmap);
   bitmap_layer_set_compositing_mode(s_layer, GCompOpSet);
   layer_add_child(root, bitmap_layer_get_layer(s_layer));
